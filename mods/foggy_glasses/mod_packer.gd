67a739a5021245a67d74458173e6df9548725219
Xanderath <alexgiller@proton.me>
2026-06-27 23:44:57 -0400

    "super duper fixed" the foggy glasses' mod_packer

--- mods/foggy_glasses/mod_packer.gd
+++ mods/foggy_glasses/mod_packer.gd
@@ -24,7 +24,10 @@ var files: Array[String] = [
 	"res://mods/foggy_glasses/aqua/swing.wav.import",
 	"res://mods/foggy_glasses/aqua/miniboss_new_section_idea_wip.ogg", #aqua fight musi
 	"res://mods/foggy_glasses/aqua/miniboss_new_section_idea_wip.ogg.import", #aqua fight musi
-
+	
+	"res://mods/foggy_glasses/pronounpalace-slashtiletype-px.png", #wooden slash intent icon
+	"res://strings/intent/slash_wood.txt", #wood slash intent strings
+	
 	"res://mods/foggy_glasses/dew_jubilist_icons.png",
 	"res://arte/characters/dew_jubilist.png",
 	"res://arte/characters/dew_jubilist_trans.png",
@@ -47,6 +50,13 @@ func _run() -> void :
 			if loaded is CompressedTexture2D:
 				packer.add_file(loaded.load_path, loaded.load_path)
 				packer.add_file(file + ".import", file + ".import")
+			elif loaded is AudioStream:
+				packer.add_file(file + ".import", file + ".import")
+				var config:=ConfigFile.new()
+				config.load(file + ".import")
+				var imported_path=config.get_value("remap","path")
+				packer.add_file(imported_path, imported_path)
+				#print(imported_path)
 			else:
 				packer.add_file(file, file)
 		else:
